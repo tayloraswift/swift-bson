f161636bc5b1efd0b76d9dd098f12c33a5f184b2
Dianna Ma <mdianna1989@gmail.com>
2024-11-26 22:49:46 +0000

    make BinaryArray settable

--- Sources/BSONABI/Views/BSON.BinaryArray.swift
+++ Sources/BSONABI/Views/BSON.BinaryArray.swift
@@ -3,16 +3,14 @@ extension BSON
     /// A `BinaryArray` is a typed view of an ``ArraySlice`` as a densely-packed buffer of
     /// trivial `Element`s. It’s a good idea to use something unpretentious like a tuple of
     /// fixed-width integers for the `Element` type, to avoid unexpected padding behavior.
-    ///
-    /// In any large MongoDB deployment, you should assume that your schema will be loaded and
-    /// saved from machines with different endianness.
+
     @frozen public
     struct BinaryArray<Element> where Element:BitwiseCopyable
     {
         public
-        let bytes:ArraySlice<UInt8>
+        var bytes:ArraySlice<UInt8>
         public
-        let count:Int
+        var count:Int
 
         @inlinable
         init(bytes:ArraySlice<UInt8>, count:Int)
@@ -40,15 +38,42 @@ extension BSON.BinaryArray:RandomAccessCollection
     @inlinable public
     var endIndex:Int { self.count }
 
+    /// Accesses the element at the specified position.
+    ///
+    /// In any large MongoDB deployment, you should assume that your schema will be loaded and
+    /// saved from machines with different endianness.
+    ///
+    /// This means that if the `Element` type is (or contains) an integer larger than a byte,
+    /// you should explicitly specify the endianness when getting or setting through the
+    /// subscript.
+    ///
+    /// For example, if you are coding with a tuple `(UInt32, UInt16)`, you should load the
+    /// logical values as `(x, y) = (UInt32.init(bigEndian: $0), UInt16.init(bigEndian: $1))`
+    /// and store them as `($0, $1) = (x.bigEndian, y.bigEndian)`.
     @inlinable public
     subscript(position:Int) -> Element
     {
-        precondition(self.indices ~= position, "Index out of bounds")
-
-        return self.bytes.withUnsafeBytes
+        get
         {
-            $0.loadUnaligned(fromByteOffset: position * MemoryLayout<Element>.size,
-                as: Element.self)
+            precondition(self.indices ~= position, "Index out of bounds")
+
+            return self.bytes.withUnsafeBytes
+            {
+                $0.loadUnaligned(
+                    fromByteOffset: position * MemoryLayout<Element>.size,
+                    as: Element.self)
+            }
+        }
+        set(value)
+        {
+            precondition(self.indices ~= position, "Index out of bounds")
+
+            self.bytes.withUnsafeMutableBytes
+            {
+                $0.storeBytes(of: value,
+                    toByteOffset: position * MemoryLayout<Element>.size,
+                    as: Element.self)
+            }
         }
     }
 }
