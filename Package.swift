ea18a389ad9420f14316380e3e25bb8dcddaa838
Dianna Ma <mdianna1989@gmail.com>
2024-11-27 04:33:44 +0000

    platform mins

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package:Package = .init(name: "swift-bson",
-    platforms: [.macOS(.v15), .iOS(.v17), .tvOS(.v17), .visionOS(.v1), .watchOS(.v10)],
+    platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18), .visionOS(.v2), .watchOS(.v11)],
     products: [
         .library(name: "BSON", targets: ["BSON"]),
         .library(name: "BSONLegacy", targets: ["BSONLegacy"]),
