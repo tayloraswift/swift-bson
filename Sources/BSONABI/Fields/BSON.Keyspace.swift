a6e08f38e340a98193e7f1f3378cea3726c3ba1a
Dianna Ma <mdianna1989@gmail.com>
2025-01-14 23:25:23 +0000

    document Keyspace

--- Sources/BSONABI/Fields/BSON.Keyspace.swift
+++ Sources/BSONABI/Fields/BSON.Keyspace.swift
@@ -1,5 +1,12 @@
 extension BSON
 {
+    /// A keyspace is a type that is used to identify an arbitrary associative mapping within
+    /// a ``BSON.Document``. Keyspaces are not the same as schema keys, which are a fixed (but
+    /// evolving) set of names that are used to identify structural properties in a model type.
+    ///
+    /// The main functional difference between a keyspace and a schema key is that failing to
+    /// convert a string key to a keyspace is an error, but failing to convert a string key to
+    /// a schema key is not.
     public
     protocol Keyspace:RawRepresentable<String>, Sendable
     {
