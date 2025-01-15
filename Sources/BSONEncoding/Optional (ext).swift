5541f776d3f886b9a41dc98dcdb25575814af0d7
Dianna Ma <mdianna1989@gmail.com>
2025-01-15 01:27:42 +0000

    remove the last vestigial encoding API

--- Sources/BSONEncoding/Optional (ext).swift
+++ Sources/BSONEncoding/Optional (ext).swift
@@ -7,29 +7,3 @@ extension Optional:BSONEncodable where Wrapped:BSONEncodable
         self?.encode(to: &field) ?? BSON.Null.init().encode(to: &field)
     }
 }
-//  These APIs must additionally be extensions on ``Optional`` and not just
-//  ``BSONEncodable`` because SE-299 does not support protocol extension
-//  member lookup with unnamed closure parameters. Only the APIs that take
-//  closure arguments need to be duplicated here.
-extension BSON.Document?
-{
-    @inlinable public
-    init<CodingKey>(_:CodingKey.Type = CodingKey.self,
-        with populate:(inout BSON.DocumentEncoder<CodingKey>) throws -> ()) rethrows
-    {
-        self = .some(try .init(with: populate))
-    }
-    @inlinable public
-    init(with populate:(inout BSON.DocumentEncoder<BSON.Key>) throws -> ()) rethrows
-    {
-        self = .some(try .init(with: populate))
-    }
-}
-extension BSON.List?
-{
-    @inlinable public
-    init(with populate:(inout BSON.ListEncoder) throws -> ()) rethrows
-    {
-        self = .some(try .init(with: populate))
-    }
-}
