b8398f628bdba80513458d32f6c77bf7229555d5
Dianna Ma <mdianna1989@gmail.com>
2025-01-15 01:35:40 +0000

    split Dictionary and Set conformances into encoding and decoding halves

--- Sources/BSONDecoding/Set (ext).swift
+++ Sources/BSONDecoding/Set (ext).swift
@@ -1,14 +1,5 @@
-extension Set:BSONEncodable, BSONListEncodable where Element:BSONEncodable
-{
-    @inlinable public
-    func encode(to bson:inout BSON.ListEncoder)
-    {
-        for element:Element in self
-        {
-            element.encode(to: &bson[+])
-        }
-    }
-}
+import BSONABI
+
 extension Set:BSONDecodable, BSONListDecodable where Element:BSONDecodable
 {
     @inlinable public
